8f1021c2f045ae4592b1a6d5bb5b1910db88645a
Himanshu Pahadia <himanshu14045>
2016-10-13 16:51:12 +0530

    Lifecycle methods added

--- app/src/main/java/com/safety/hss/safetyfirstdata/MainActivity.java
+++ app/src/main/java/com/safety/hss/safetyfirstdata/MainActivity.java
@@ -9,7 +9,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         // Linking my layout file - checking git push
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
